2800318dc29900f6f9e10e01e5ee8d1d5de63c92
Happy (Hang Jie) <hangjie.kreloses@outlook.com>
2023-12-28 19:09:22 +0800

    Prebuild dist so that it can be used straight without npm

---
+++ dist/main.umd.js
@@ -0,0 +1,264 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("highlight-words-core"), require("prop-types"), require("react"), require("memoize-one"));
+	else if(typeof define === 'function' && define.amd)
+		define(["highlight-words-core", "prop-types", "react", "memoize-one"], factory);
+	else if(typeof exports === 'object')
+		exports["react-highlight-words"] = factory(require("highlight-words-core"), require("prop-types"), require("react"), require("memoize-one"));
+	else
+		root["react-highlight-words"] = factory(root["highlight-words-core"], root["prop-types"], root["react"], root["memoize-one"]);
+})(this, function(__WEBPACK_EXTERNAL_MODULE_3__, __WEBPACK_EXTERNAL_MODULE_4__, __WEBPACK_EXTERNAL_MODULE_5__, __WEBPACK_EXTERNAL_MODULE_6__) {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+	
+	var _Highlighter = __webpack_require__(2);
+	
+	var _Highlighter2 = _interopRequireDefault(_Highlighter);
+
+	exports['default'] = _Highlighter2['default'];
+	module.exports = exports['default'];
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
+	
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+	
+	exports['default'] = Highlighter;
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+	
+	function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+	
+	var _highlightWordsCore = __webpack_require__(3);
+	
+	var _propTypes = __webpack_require__(4);
+	
+	var _propTypes2 = _interopRequireDefault(_propTypes);
+	
+	var _react = __webpack_require__(5);
+	
+	var _memoizeOne = __webpack_require__(6);
+	
+	var _memoizeOne2 = _interopRequireDefault(_memoizeOne);
+	
+	Highlighter.propTypes = {
+	  activeClassName: _propTypes2['default'].string,
+	  activeIndex: _propTypes2['default'].number,
+	  activeStyle: _propTypes2['default'].object,
+	  autoEscape: _propTypes2['default'].bool,
+	  className: _propTypes2['default'].string,
+	  findChunks: _propTypes2['default'].func,
+	  highlightClassName: _propTypes2['default'].oneOfType([_propTypes2['default'].object, _propTypes2['default'].string]),
+	  highlightStyle: _propTypes2['default'].object,
+	  highlightTag: _propTypes2['default'].oneOfType([_propTypes2['default'].node, _propTypes2['default'].func, _propTypes2['default'].string]),
+	  sanitize: _propTypes2['default'].func,
+	  searchWords: _propTypes2['default'].arrayOf(_propTypes2['default'].oneOfType([_propTypes2['default'].string, _propTypes2['default'].instanceOf(RegExp)])).isRequired,
+	  textToHighlight: _propTypes2['default'].string.isRequired,
+	  unhighlightTag: _propTypes2['default'].oneOfType([_propTypes2['default'].node, _propTypes2['default'].func, _propTypes2['default'].string]),
+	  unhighlightClassName: _propTypes2['default'].string,
+	  unhighlightStyle: _propTypes2['default'].object
+	};
+	
+	/**
+	 * Highlights all occurrences of search terms (searchText) within a string (textToHighlight).
+	 * This function returns an array of strings and <span>s (wrapping highlighted words).
+	 */
+	
+	function Highlighter(_ref) {
+	  var _ref$activeClassName = _ref.activeClassName;
+	  var activeClassName = _ref$activeClassName === undefined ? '' : _ref$activeClassName;
+	  var _ref$activeIndex = _ref.activeIndex;
+	  var activeIndex = _ref$activeIndex === undefined ? -1 : _ref$activeIndex;
+	  var activeStyle = _ref.activeStyle;
+	  var autoEscape = _ref.autoEscape;
+	  var _ref$caseSensitive = _ref.caseSensitive;
+	  var caseSensitive = _ref$caseSensitive === undefined ? false : _ref$caseSensitive;
+	  var className = _ref.className;
+	  var findChunks = _ref.findChunks;
+	  var _ref$highlightClassName = _ref.highlightClassName;
+	  var highlightClassName = _ref$highlightClassName === undefined ? '' : _ref$highlightClassName;
+	  var _ref$highlightStyle = _ref.highlightStyle;
+	  var highlightStyle = _ref$highlightStyle === undefined ? {} : _ref$highlightStyle;
+	  var _ref$highlightTag = _ref.highlightTag;
+	  var highlightTag = _ref$highlightTag === undefined ? 'mark' : _ref$highlightTag;
+	  var sanitize = _ref.sanitize;
+	  var searchWords = _ref.searchWords;
+	  var textToHighlight = _ref.textToHighlight;
+	  var _ref$unhighlightTag = _ref.unhighlightTag;
+	  var unhighlightTag = _ref$unhighlightTag === undefined ? 'span' : _ref$unhighlightTag;
+	  var _ref$unhighlightClassName = _ref.unhighlightClassName;
+	  var unhighlightClassName = _ref$unhighlightClassName === undefined ? '' : _ref$unhighlightClassName;
+	  var unhighlightStyle = _ref.unhighlightStyle;
+	
+	  var rest = _objectWithoutProperties(_ref, ['activeClassName', 'activeIndex', 'activeStyle', 'autoEscape', 'caseSensitive', 'className', 'findChunks', 'highlightClassName', 'highlightStyle', 'highlightTag', 'sanitize', 'searchWords', 'textToHighlight', 'unhighlightTag', 'unhighlightClassName', 'unhighlightStyle']);
+	
+	  var chunks = (0, _highlightWordsCore.findAll)({
+	    autoEscape: autoEscape,
+	    caseSensitive: caseSensitive,
+	    findChunks: findChunks,
+	    sanitize: sanitize,
+	    searchWords: searchWords,
+	    textToHighlight: textToHighlight
+	  });
+	  var HighlightTag = highlightTag;
+	  var highlightIndex = -1;
+	  var highlightClassNames = '';
+	  var highlightStyles = undefined;
+	
+	  var lowercaseProps = function lowercaseProps(object) {
+	    var mapped = {};
+	    for (var key in object) {
+	      mapped[key.toLowerCase()] = object[key];
+	    }
+	    return mapped;
+	  };
+	  var memoizedLowercaseProps = (0, _memoizeOne2['default'])(lowercaseProps);
+	
+	  return (0, _react.createElement)('span', _extends({
+	    className: className
+	  }, rest, {
+	    children: chunks.map(function (chunk, index) {
+	      var text = textToHighlight.substr(chunk.start, chunk.end - chunk.start);
+	
+	      if (chunk.highlight) {
+	        highlightIndex++;
+	
+	        var highlightClass = undefined;
+	        if (typeof highlightClassName === 'object') {
+	          if (!caseSensitive) {
+	            highlightClassName = memoizedLowercaseProps(highlightClassName);
+	            highlightClass = highlightClassName[text.toLowerCase()];
+	          } else {
+	            highlightClass = highlightClassName[text];
+	          }
+	        } else {
+	          highlightClass = highlightClassName;
+	        }
+	
+	        var isActive = highlightIndex === +activeIndex;
+	
+	        highlightClassNames = highlightClass + ' ' + (isActive ? activeClassName : '');
+	        highlightStyles = isActive === true && activeStyle != null ? Object.assign({}, highlightStyle, activeStyle) : highlightStyle;
+	
+	        var props = {
+	          children: text,
+	          className: highlightClassNames,
+	          key: index,
+	          style: highlightStyles
+	        };
+	
+	        // Don't attach arbitrary props to DOM elements; this triggers React DEV warnings (https://fb.me/react-unknown-prop)
+	        // Only pass through the highlightIndex attribute for custom components.
+	        if (typeof HighlightTag !== 'string') {
+	          props.highlightIndex = highlightIndex;
+	        }
+	
+	        return (0, _react.createElement)(HighlightTag, props);
+	      } else {
+	        return (0, _react.createElement)(unhighlightTag, {
+	          children: text,
+	          className: unhighlightClassName,
+	          key: index,
+	          style: unhighlightStyle
+	        });
+	      }
+	    })
+	  }));
+	}
+	
+	module.exports = exports['default'];
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+	module.exports = __WEBPACK_EXTERNAL_MODULE_3__;
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+	module.exports = __WEBPACK_EXTERNAL_MODULE_4__;
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
+	module.exports = __WEBPACK_EXTERNAL_MODULE_5__;
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+	module.exports = __WEBPACK_EXTERNAL_MODULE_6__;
+
+/***/ })
+/******/ ])
+});
+;
+//# sourceMappingURL=main.umd.js.map
